3bbeaf49a70a5f6393255e2965ca5ee11872084f
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-08-15 23:59:37 +0300

    feat: Add signup endpoint to auth controller

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiProperty, ApiTags } from '@nestjs/swagger';
 import { SigninDto } from './dto/signin.dto';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @ApiTags('Auth Resource')
 @Controller('auth')
@@ -11,5 +12,10 @@ export class AuthController {
     @Post('signin')
     async signIn(@Body() signinDto: SigninDto) {
         return this.authService.signIn(signinDto);
+    }
+
+    @Post('signup')
+    async signUp(@Body() signupDto: CreateUserDto) {
+        return this.authService.signUp(signupDto);
     }
 }
