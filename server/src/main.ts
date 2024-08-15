3228e01e40189124b722e677db5986a169d25ccb
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-08-15 23:59:14 +0300

    feat: Enable API versioning with URI prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,17 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('TODO Application')
-    .setDescription('TODO')
-    .setVersion('1.0')
-    .addTag('TODO')
+    .setTitle('Task Application')
+    .setDescription('Task Application API')
+    .setVersion('1.0').
+    addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'header',
+        })
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, document);
+  app.enableVersioning({
+    type:VersioningType.URI,
+    prefix:'v',
+  });
   await app.listen(5001);
 }
 bootstrap();
