3c76934bde39aedd6ee08486cceffe218711eaec
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-08-15 18:06:06 +0300

    feat: add swagger document

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,13 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config= new DocumentBuilder().
-  setTitle('API').
-  setDescription('API').
-  setVersion('1.0').
-  build();
-  const document= SwaggerModule.createDocument(app,config);
-  SwaggerModule.setup('api',app,document);
+  const config = new DocumentBuilder()
+    .setTitle('TODO Application')
+    .setDescription('TODO')
+    .setVersion('1.0')
+    .addTag('TODO')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
   await app.listen(5001);
 }
 bootstrap();
