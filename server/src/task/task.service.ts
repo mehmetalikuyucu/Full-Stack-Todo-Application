48d9ab47906485c2b7fa8f2598adb2dd90911701
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-08-16 11:44:29 +0300

    fix: skip is not a number issue

--- server/src/task/task.service.ts
+++ server/src/task/task.service.ts
@@ -5,34 +5,49 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { Repository } from 'typeorm';
 import { GetTasksDto } from './dto/get-tasks.dto';
+import { Console } from 'console';
 
 @Injectable()
 export class TaskService {
-  
-  constructor(@InjectRepository(Task) private taskRepository:Repository<Task>) {}
+  constructor(
+    @InjectRepository(Task) private taskRepository: Repository<Task>,
+  ) {}
 
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
     const newTask = this.taskRepository.create(createTaskDto);
     return await this.taskRepository.save(newTask);
   }
 
-  async findAll(getTasksDto:GetTasksDto): Promise<{data:Task[],total:number}> {
-    const {page,limit,sortBy,sortOrder,title,status,userId} = getTasksDto;
+  async findAll(
+    getTasksDto: GetTasksDto,
+  ): Promise<{ data: Task[]; total: number }> {
+    const {
+      page = 1,
+      limit = 10,
+      sortBy,
+      sortOrder,
+      title,
+      status,
+      userId,
+    } = getTasksDto;
     const query = this.taskRepository.createQueryBuilder('task');
-    if(title){
-      query.andWhere('task.title like :title',{title:`%${title}%`});
+    if (title) {
+      query.andWhere('task.title like :title', { title: `%${title}%` });
     }
-    if(status){
-      query.andWhere('task.status = :status',{status});
+    if (status) {
+      query.andWhere('task.status = :status', { status });
     }
-    if(sortBy){
-      query.orderBy(`task.${sortBy}`,sortOrder);
+    if (sortBy) {
+      query.orderBy(`task.${sortBy}`, sortOrder);
     }
-    if(userId){
-      query.andWhere('task.userId = :userId',{userId});
+    if (userId) {
+      query.andWhere('task.userId = :userId', { userId });
     }
-    const [data,total] = await query.skip((page-1)*limit).take(limit).getManyAndCount();
-    return {data,total};
+    const [data, total] = await query
+      .skip((page - 1) * limit)
+      .take(limit)
+      .getManyAndCount();
+    return { data, total };
   }
 
   async findOne(id: string): Promise<Task> {
